b12b377861624a16c007881b35e0a0f738dc26ea
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2021-10-13 19:54:45 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ para1 = float(input("Podaj pierwszy parametr: "))
 para2 = float(input("Podaj drugi parametr: "))
 para3 = float(input("Podaj trzeci parametr: "))
 
-parametrFinal = para1 * para2 * para3
+parametrFinal = para1 + para2 + para3
 
 if para1 == para2 and para2 == para3:
     print("Wynik koncowy: {}".format(parametrFinal * 2))
